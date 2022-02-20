47a612c8d9c6eddf8c55c963ed6809e7fdb9d0df
elhamhsnpr <elhamhassanpour133@gmail.com>
2022-02-20 17:44:49 +0100

    chg: config ajv as json validator      set errorhandeling

--- src/functions/trackUser/handler.ts
+++ src/functions/trackUser/handler.ts
@@ -1,24 +1,50 @@
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
 import { formatJSONResponse } from '@libs/api-gateway';
 import { middyfy } from '@libs/lambda';
+import Ajv from 'ajv';
 // import 'source-map-support/register';
 
 
 import schema from './schema';
 
 const handler: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
+    try {
 
-    const body = event.body;
+        const ajv = new Ajv();
 
-    const trackUserBody = {
-        received: true,
-        ...body
+        const body = event.body;
 
+        const validate = ajv.compile(schema);
+        const valid = validate(body)
+
+        if (valid) {
+
+            const trackUserBody = {
+                received: true,
+                ...body
+
+            }
+
+
+            return formatJSONResponse({
+                trackUserBody
+            });
+
+        } else {
+
+            return formatJSONResponse({
+                statusCode: 400,
+                body: ajv.errorsText(validate.errors)
+            });
+
+        }
+
+    } catch (error) {
+        return formatJSONResponse({
+            statusCode: 500,
+            body: error
+        });
     }
-    return formatJSONResponse({
-        trackUserBody
-    });
 };
 
 export const main = middyfy(handler);
-// module.exports = serverlessConfiguration;
